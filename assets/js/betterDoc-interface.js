7b6a40f33fcaaf814ecdf3e0b78133ff26cc1536
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-02-16 23:46:20 -0800

    Updated UI. Added a new Object array in betterDoc-interface to store data returned from our api call.

--- assets/js/betterDoc-interface.js
+++ assets/js/betterDoc-interface.js
@@ -1,20 +1,19 @@
 import { BetterDoc } from '../assets/js/betterDoc.js';
 
-
 $(() => {
-let betterDoc = new BetterDoc();
-
-  // betterDoc.request('name', 'suhler');
-
+  let betterDoc = new BetterDoc();
+  let doctors = {};
+  
   betterDoc.request('name','johnson').then((response) => {
     let body = JSON.parse(response);
-    console.log(body);
-    console.log(body.data.length);
 
-    
-
+    for (let i = 0; i < body.data.length; i++) {
+      doctors[i] = {
+        practices: body.data[i].practices,
+        profile: body.data[i].profile
+      };
+    }
   });
 
-
-
+  console.log(doctors);
 });
